626e8b46e8b3fddaae0961e39f241a50cb458e78
Jensen Mei <jensenmei804@gmail.com>
2020-07-14 13:25:52 -0700

    replace static bookshelf books and create BookList component

---
+++ src/BookList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+
+  render() {
+    console.log(this.props.books);
+    return (
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {this.props.books.map((book) => (
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="move" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                {book.authors.map((author) => (
+                  <div key={author} className="book-authors">{author}</div>
+                ))}
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
+    )
+  }
+}
+
+export default BookList;
