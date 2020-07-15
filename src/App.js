2eabbe1842739a9e35b0664997d4a77425ce3ee9
Jensen Mei <jensenmei804@gmail.com>
2020-07-14 17:17:41 -0700

    Implement logic to update books on the shelf

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from './BookList'
+import SearchBooks from './SearchBooks'
+import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
@@ -11,46 +14,44 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    books: [],
-    showSearchPage: false
+    books: []
   }
 
   componentDidMount() {
     BooksAPI.getAll()
       .then((books) => {
         this.setState(() => ({
-          books
+          books: books
         }))
         console.log("All Books", books);
       })
   }
 
+  updateShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+
+    const updatedBooks = this.state.books.map((origBook) => {
+      if(origBook.id === book.id) {
+        const updatedBook = {
+          ...origBook,
+          shelf: shelf
+        }
+        return updatedBook;
+      }
+      return origBook;
+    })
+
+    console.log("updated books", updatedBooks)
+    this.setState((prevState) => ({
+      books: updatedBooks
+    }))
+    console.log("New State", this.state.books)
+  }
+
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */
-                }
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+        <Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -59,23 +60,26 @@ class BooksApp extends React.Component {
               <div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
-                  <BookList books={this.state.books.filter((book) => (book.shelf === "currentlyReading"))}/>
+                  <BookList onShelfUpdate={this.updateShelf} books={this.state.books.filter((book) => (book.shelf === "currentlyReading"))}/>
                 </div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
-                  <BookList books={this.state.books.filter((book) => (book.shelf === "wantToRead"))}/>
+                  <BookList onShelfUpdate={this.updateShelf} books={this.state.books.filter((book) => (book.shelf === "wantToRead"))}/>
                 </div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Read</h2>
-                  <BookList books={this.state.books.filter((book) => (book.shelf === "read"))}/>
+                  <BookList onShelfUpdate={this.updateShelf} books={this.state.books.filter((book) => (book.shelf === "read"))}/>
                 </div>
               </div>
             </div>
             <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+              <Link to="/search">
+                <button>Add a book</button>
+              </Link>
             </div>
           </div>
-        )}
+        )} />
+        <Route path='/search' component={SearchBooks}/>
       </div>
     )
   }
--- src/BookList.js
+++ src/BookList.js
@@ -13,7 +13,7 @@ class BookList extends Component {
                 <div className="book-top">
                   <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
                   <div className="book-shelf-changer">
-                    <select>
+                    <select value="move" onChange={(event) => this.props.onShelfUpdate(book, event.target.value)}>
                       <option value="move" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
                       <option value="wantToRead">Want to Read</option>
